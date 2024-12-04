ead934ed7e89f6251b0e765f9a73ec8a86ff3e5e
huzh <huzh@qutke.com>
2024-12-04 11:16:11 +0800

    bump version to v24.1204.1116

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
-import App from './App.vue'
+import App from './app.vue'
 import router from '@/router'
 
 const app = createApp(App)
