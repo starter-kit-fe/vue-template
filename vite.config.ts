e19df8cd567e81c1cd28921df57a7bcbaca616ab
huzh <huzh@qutke.com>
2025-01-14 15:07:59 +0800

    bump version to v25.0114.1507

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,16 @@ import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import { dayjs } from "element-plus";
 import tailwindcss from "@tailwindcss/vite";
+import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
-  process.env.VITE_APP_BUILD_TIME = dayjs().format("YYYY-MM-DD HH:mm:ss");
   return {
     base: env.VITE_APP_BASE_URL,
+    define: {
+      __BUILD_TIME__: `'${dayjs().format("YYYY-MM-DD HH:mm:ss")}'`,
+    },
     plugins: [
       vue(),
       // vueDevTools(),
@@ -25,10 +28,34 @@ export default defineConfig(({ command, mode }) => {
         resolvers: [ElementPlusResolver()],
       }),
       tailwindcss(),
+      compression({
+        ext: ".gz",
+        deleteOriginFile: false,
+      }),
+      //   compression({
+      //     ext: '.br',
+      //     algorithm: 'brotliCompress',
+      //     deleteOriginFile: false
+      // })
     ],
     css: {
       preprocessorOptions: {
         scss: { api: "modern-compiler" },
+      },
+      //fix:error:stdin>:7356:1: warning: "@charset" must be the first rule in the file
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: "internal:charset-removal",
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === "charset") {
+                  atRule.remove();
+                }
+              },
+            },
+          },
+        ],
       },
     },
     resolve: {
